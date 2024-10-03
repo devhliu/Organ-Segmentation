3ac1785195345141d5c9ef8b729869bd33dbe44d
Yazdan Salimi <56175580+YazdanSalimi@users.noreply.github.com>
2024-10-03 11:28:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='inference_code',
-    version='1.0',
+    name="Organ-Segmentation",  # Replace with your package name
+    version="0.1.0",
+    author="Yazdan Salimi",
+    author_email="salimiyazdan@gmail.com",
+    description="Multi Modality Organ Segmentation tools",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/YazdanSalimi/Organ-Segmentation",
     packages=find_packages(),
-    install_requires=[glob, SimpleITK, termcolor, tqdm, shutil, pandas, multiprocessing, natsort, nnunetv2],  # Add any dependencies here
-    entry_points={
-        'console_scripts': [
-            'run_inference=inference.file1:main',  # Maps command to script
-        ],
-    },
+    py_modules=["inference", "ImageUtilities"],
+    install_requires=[
+        # List your dependencies here
+        # e.g., 'numpy', 'pandas'
+        "shutil", "pandas", "tqdm",
+        "termcolor", "glob", "torch",
+        "nnunetv2", "SimpleITK", "multiprocessing",
+        "numpy", "natsort",        
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # You can change the license if necessary
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Specify the Python version
 )
